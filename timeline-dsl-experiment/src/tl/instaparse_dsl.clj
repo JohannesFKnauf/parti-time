a82758de373c48d93f2f57e4265c4004b463c037
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-19 07:30:38 +0200

    generalize timeline parser to enable use for arbitrary timeline dimensions

--- timeline-dsl-experiment/src/tl/instaparse_dsl.clj
+++ timeline-dsl-experiment/src/tl/instaparse_dsl.clj
@@ -1,7 +1,7 @@
 (ns tl.instaparse-dsl
   (:require [instaparse.core :as insta]))
 
-(insta/defparser work-tl
+(insta/defparser timeline-parser
   "./resources/tl/instaparse_grammar.ebnf")
 
 (defn parsed->timeline [parsing-result]
--- timeline-dsl-experiment/test/tl/instaparse_dsl_test.clj
+++ timeline-dsl-experiment/test/tl/instaparse_dsl_test.clj
@@ -2,18 +2,18 @@
   (:require [tl.instaparse-dsl :as sut]
             [clojure.test :as t]))
 
-(t/deftest work-tl-parser
+(t/deftest timeline-parser
   (t/testing "Entry parsing"
     (t/is (= [:timeline
               [:entry
-               [:isodate "2019-02-03"]
-               [:start-on-project
+               [:reference-date "2019-02-03"]
+               [:time-partition-start
                 [:hhmm-time "1215"]
-                [:project-identifier "Some Project"]]
-               [:occupation [:occupation-description "Something to do"]]]]
-             (sut/work-tl (str "2019-02-03\n"
-                               "1215 book on Some Project\n"
-                               "             Something to do"))))))
+                [:subject "Some Project"]]
+               [:detail "Something to do"]]]
+             (sut/timeline-parser (str "2019-02-03\n"
+                                       "1215 Some Project\n"
+                                       "     Something to do"))))))
 
 (t/deftest timeline-transformation
   )
