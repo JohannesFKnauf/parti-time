0275d56e33faae0786f81f8c7b0aec395946c425
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2019-09-19 08:22:00 +0200

    timeline parser: add entry transformation, adjust grammar to simplify parsing result even further

--- timeline-dsl-experiment/src/tl/instaparse_dsl.clj
+++ timeline-dsl-experiment/src/tl/instaparse_dsl.clj
@@ -1,8 +1,21 @@
 (ns tl.instaparse-dsl
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [java-time]))
 
 (insta/defparser timeline-parser
   "./resources/tl/instaparse_grammar.ebnf")
 
-(defn parsed->timeline [parsing-result]
+(defn ast->entries
+  "Transforms the abstract syntax tree of a timeline DSL parsing result into a proper timeline."
+  ([ast]
+   (insta/transform {:reference-date #(hash-map :reference-date (java-time/local-date "yyyy-MM-dd" %))
+                     :hhmm-time #(hash-map :time (java-time/local-time "HHmm" %))
+                     :subject #(hash-map :subject %)
+                     :detail #(hash-map :detail %)
+                     :entry merge
+                     :timeline vector}
+                    ast)))
+
+
+(defn work-timeline [parsed-timeline]
   )
--- timeline-dsl-experiment/test/tl/instaparse_dsl_test.clj
+++ timeline-dsl-experiment/test/tl/instaparse_dsl_test.clj
@@ -1,19 +1,68 @@
 (ns tl.instaparse-dsl-test
   (:require [tl.instaparse-dsl :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [java-time]))
 
 (t/deftest timeline-parser
   (t/testing "Entry parsing"
     (t/is (= [:timeline
               [:entry
                [:reference-date "2019-02-03"]
-               [:time-partition-start
-                [:hhmm-time "1215"]
-                [:subject "Some Project"]]
+               [:hhmm-time "1215"]
+               [:subject "Some Project"]
                [:detail "Something to do"]]]
              (sut/timeline-parser (str "2019-02-03\n"
                                        "1215 Some Project\n"
-                                       "     Something to do"))))))
+                                       "     Something to do")))
+          "Single sane entry")
+    (t/is (= [:timeline
+              [:entry
+               [:reference-date "2019-02-03"]
+               [:hhmm-time "1215"]
+               [:subject "Some Project"]
+               [:detail "Something to do"]]
+              [:entry
+               [:hhmm-time "1515"]
+               [:subject "Some other Project"]
+               [:detail "Something else to do"]]]
+             (sut/timeline-parser (str "2019-02-03\n"
+                                       "1215 Some Project\n"
+                                       "     Something to do\n"
+                                       "1515 Some other Project\n"
+                                       "     Something else to do")))
+          "Additional entry without reference date")
+    (t/is (= [:timeline
+              [:entry
+               [:reference-date "2019-02-03"]
+               [:hhmm-time "1215"]
+               [:subject "Some Project"]
+               [:detail "Something to do"]
+               [:detail "Something else to do"]]]
+             (sut/timeline-parser (str "2019-02-03\n"
+                                       "1215 Some Project\n"
+                                       "     Something to do\n"
+                                       "     Something else to do")))
+          "Additional detail")
+    (t/is (= [:timeline
+              [:entry
+               [:reference-date "2019-02-03"]
+               [:hhmm-time "1215"]
+               [:subject "Some Project"]]]
+             (sut/timeline-parser (str "2019-02-03\n"
+                                       "1215 Some Project")))
+          "Entry without detail")))
 
 (t/deftest timeline-transformation
-  )
+  (t/testing "Conversion to proper hash-map"
+    (t/is (= [{:reference-date (java-time/local-date "2019-02-03")
+               :time (java-time/local-time "12:15")
+               :subject "Some Project"
+               :detail "Something to do"}]
+             (sut/ast->entries
+              [:timeline
+               [:entry
+                [:reference-date "2019-02-03"]
+                [:hhmm-time "1215"]
+                [:subject "Some Project"]
+                [:detail "Something to do"]]])))))
+
