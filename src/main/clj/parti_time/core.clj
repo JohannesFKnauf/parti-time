dbcffa4972b8ee7cd882f410e2718d913101c17a
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2021-05-03 08:36:43 +0200

    fix #1: Improve output for time-ordering violations

--- src/main/clj/parti_time/core.clj
+++ src/main/clj/parti_time/core.clj
@@ -5,7 +5,7 @@
   (let [{start-time :start-time} time-frame
         {end-time :start-time} next-time-frame]
     (when (time/date-time-before? end-time start-time)
-      (throw (java.lang.IllegalArgumentException. "End time predates start time. Times must be strictly ordered.")))
+      (throw (java.lang.IllegalArgumentException. (str "End time '" end-time "' predates start time '" start-time "'. Times must be strictly ordered."))))
     (assoc time-frame
            :end-time end-time
            :duration-minutes (time/minutes-between start-time end-time))))
