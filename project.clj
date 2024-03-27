1a1c574023d3ed275d7cdbdcb513bced55de4a7e
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2024-03-27 22:09:31 +0100

    provide first version of #4 google sheets integration

--- project.clj
+++ project.clj
@@ -5,10 +5,14 @@
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.json "2.4.0"]
+                 [org.clojure/test.check "1.1.1"]
                  [org.flatland/ordered "1.15.11"]
                  [io.forward/yaml "1.0.11"]
                  [cli-matic "0.5.4"]
                  [clojure-csv "2.0.2"]
+                 [com.google.api-client/google-api-client "2.3.0"]
+                 [com.google.oauth-client/google-oauth-client-jetty "1.34.1"]
+                 [com.google.apis/google-api-services-sheets "v4-rev20230815-2.0.0"]
                  [instaparse "1.4.12"]
                  [medley "1.4.0"]]
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
@@ -27,9 +31,32 @@
                  :opts ["--verbose"
                         "--initialize-at-build-time"
                         "--report-unsupported-elements-at-runtime"
-                        "-H:+ReportExceptionStackTraces"
+                        "-march=x86-64"
+                        ~(str "--initialize-at-run-time="
+                              (clojure.string/join ","
+                                                   ["sun.awt.dnd.SunDropTargetContextPeer$EventDispatcher"
+                                                    "sun.awt.X11.XDataTransferer"
+                                                    "sun.awt.X11.XDnDConstants"
+                                                    "sun.awt.X11.MotifDnDConstants"
+                                                    "sun.awt.X11.MotifDnDConstants"
+                                                    "sun.awt.X11.WindowPropertyGetter"
+                                                    "sun.awt.X11.XWindow"
+                                                    "sun.awt.X11.XSelection"
+                                                    "sun.awt.X11.XSystemTrayPeer"
+                                                    "sun.awt.X11.XToolkitThreadBlockedHandler"
+                                                    "sun.awt.X11.XWM"
+                                                    "sun.awt.X11GraphicsConfig"
+                                                    "sun.java2d.Disposer"
+                                                    "sun.font.PhysicalStrike"
+                                                    "sun.font.StrikeCache"
+                                                    "sun.font.StrikeCache$WeakDisposerRef"
+                                                    "sun.font.SunFontManager"
+                                                    "com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp"
+                                                    "com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp$DefaultBrowser"
+                                                    "com.google.api.client.http.javanet.NetHttpTransport"]))
                         "--no-fallback"
-                        "--no-server"]}
+                        "--no-server"
+                        "--static"]}
   :profiles {:test
              {:native-image {:opts ["--report-unsupported-elements-at-runtime"
                                     "--initialize-at-build-time"
--- src/main/clj/parti_time/core.clj
+++ src/main/clj/parti_time/core.clj
@@ -14,3 +14,23 @@
   (mapv time-window
         time-line
         (drop 1 time-line)))
+
+(defn gen-time-slices [time-window next-time-window]
+  (when (time/date-time-before? (:end-time time-window) (:start-time time-window))
+    (throw (java.lang.IllegalArgumentException. (str "End time '" (:start-time next-time-window) "' predates start time '" (:end-time time-window) "'. "))))
+  (when (and (some? next-time-window)
+             (time/date-time-before? (:start-time next-time-window) (:end-time time-window)))
+    (throw (java.lang.IllegalArgumentException. (str "Subsequent start time '" (:start-time next-time-window) "' predates previous end time '" (:end-time time-window) "'. "))))
+  (into
+   [(dissoc time-window :end-time)]
+   ; fill up gaps with Private time-windows
+   (when (not= (:start-time next-time-window) (:end-time time-window))
+     [{:start-time (:end-time time-window)
+       :project "Private"}])))
+
+
+(defn time-windows->time-line [time-windows]
+  (apply concat
+         (mapv gen-time-slices
+               time-windows
+               (concat (drop 1 time-windows) [nil]))))
