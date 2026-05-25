f9c482af615aaae775989b67a3999bb7a96617f5
Johannes F. Knauf <johannes.f.knauf@metamorphant.de>
2026-05-25 21:29:53 +0200

    fix bug with timesheet/append rows and multi-day midnight overflow

--- src/main/clj/parti_time/timesheet.clj
+++ src/main/clj/parti_time/timesheet.clj
@@ -3,22 +3,50 @@
             [clojure-csv.core :as csv]
             [parti-time.core]))
 
-(defn day-record [entry]
-  (let [occupations (->> entry
-                         (:occupations)
-                         (flatten))]
-    [(time/format-time "yyyy-MM-dd" (:start-time entry))
-     (time/format-time "HH:mm" (:start-time entry))
-     (time/format-time "HH:mm" (:end-time entry))
-     ""
-     (clojure.string/join ", " occupations)
-     (:project entry)]))
+(defn start-of-next-day [date-time]
+  (.. date-time
+      (plusDays 1)
+      (toLocalDate)
+      (atStartOfDay)))
+
+(defn split-into-days [{:keys [start-time end-time]
+                        :as time-window}]
+  (lazy-seq
+   (let [next-day (start-of-next-day start-time)]
+     (if (.. end-time (isAfter next-day))
+       (let [first-day-time-window (assoc time-window
+                                          :end-time next-day)
+             remaining-time-window (assoc time-window
+                                          :start-time next-day)]
+         (cons first-day-time-window
+               (split-into-days remaining-time-window)))
+       (list time-window)))))
+
+(defn day-row
+  "day-row converts a time-window into a row of a timesheet -- for CSV export or sheet upload.
+
+  Requires end-time to be on same day or midnight-next-day -- and won't check it again."
+  [{:keys [start-time
+                       end-time
+                       occupations
+                       project]
+                :as time-window}]
+  [(time/format-time "yyyy-MM-dd" start-time)
+   (time/format-time "HH:mm" start-time)
+   (if (= end-time (start-of-next-day start-time))
+     "24:00"
+     (time/format-time "HH:mm" end-time))
+   ""
+   (clojure.string/join ", " (flatten occupations))
+   project])
 
 (defn report [time-line]
   (->> time-line
        parti-time.core/time-windows
        (filter #(not (parti-time.core/is-private? %1)))
-       (map day-record)))
+       (map split-into-days)
+       (apply concat)
+       (map day-row)))
 
 (defn csv-report [time-line]
   (->> time-line
